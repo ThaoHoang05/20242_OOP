d092707d9f2a4404c52ad9207ad6129dfbf7695e
Thao <thaohoang5836@gmail.com>
2025-02-28 21:22:16 +0700

    Commit lan dau cho lab 1

---
+++ HW.java
@@ -0,0 +1,6 @@
+public class HW{
+    public static void main(String arg[]){
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t World!");
+    }
+}
---
+++ solveEquation.java
@@ -0,0 +1,6 @@
+public class solveEquation{
+    public static void main(String[] args) {
+        String inp;
+        
+    }
+}
